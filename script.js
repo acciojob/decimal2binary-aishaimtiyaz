60c73ed0c0dde828c0aa721b3e8c131f91ed9e8c
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-03 21:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
-}
+function decimalToBinary(N) { 
+	return (N >>> 0).toString(2); 
+} 
+
+let N = 10; 
+let binary = decimalToBinary(N); 
+console.log( 
+	"The binary representation of given number is:- " + binary);
 
 window.decimalToBinary = decimalToBinary;
